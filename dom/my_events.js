3535d1aaa6fc386b6e6e0c2d1109299cc7db872e
Matheus Soares <matheusbraganca321@hotmail.com>
2023-07-01 20:09:35 -0300

    introduction to jquery

--- dom/my_events.js
+++ dom/my_events.js
@@ -1,5 +1,19 @@
-alert('one')
 let one = document.querySelector('#one')
 one.addEventListener('mouseover', function(){
     one.textContent = 'Mouse currently over'
+    one.style.color = 'red'
+})
+one.addEventListener('mouseout', function(){
+    one.textContent = 'HOOVER ME'
+    one.style.color = 'black'
+})
+let two = document.querySelector('#two')
+two.addEventListener('click',function(){
+    two.textContent = 'JUST CLICKED'
+    two.style.color = 'purple'
+})
+let three = document.querySelector('#three')
+three.addEventListener('dblclick', function(){
+    three.textContent = 'I WAS DOUBLE CLICKED';
+    three.style.color = 'yellow'
 })
